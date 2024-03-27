9ea43715f63d37c290400b38f1d14598aa726ef6
nd6k <robbie.monty@hotmail.com>
2024-03-27 21:42:18 +0000

    example skills / skillgems functions

--- src/lib.rs
+++ src/lib.rs
@@ -47,7 +47,6 @@ pub fn main() {
         .to_vec(),
     });
 
-
     println!("printing Skill Gems:");
     for gem in skill_gems {
         println!("{:#?}", gem);
@@ -58,4 +57,59 @@ pub fn main() {
         println!("{:#?}", skill);
     }
 
+}
+
+
+pub fn example_skills() -> Vec<Skill> {
+    let mut skills: Vec<Skill> = Vec::new();
+
+
+    skills.push(Skill {
+        skill_id: 0,
+        name: "fireball".to_string(),
+        description: "strike an area with a fireball doing {DAMAGE} damage.".to_string(),
+        cooldown: 12.0,
+        targeting_method: SkillTargeting::Point { range: 10.0 },
+        cast_type: CastType::Charge {
+            charge_duration: 4.0,
+            stationary_cast: true,
+        },
+        effects: [
+            Effect {
+                area_of_effect: Some(2.0),
+                varaint: EffectType::Damage(DamageEffect {
+                    true_damage: 0,
+                    melee_damage: 0,
+                    ranged_damage: 0,
+                    magic_damage: 20,
+                }),
+            },
+            Effect {
+                area_of_effect: None,
+                varaint: EffectType::BufDebuf,
+            },
+        ]
+        .to_vec(),
+    });
+
+
+    return skills;
+}
+
+
+pub fn example_skillgems() -> Vec<SkillGem> {
+    let mut skillgems: Vec<SkillGem> = Vec::new();
+
+
+    skillgems.push(SkillGem {
+        skill_id: 0,
+        item_id: 0,
+        mods: [SkillMod {
+            title: "Ignite".to_string(),
+            description: "Burns targets for extra damage over time".to_string(),
+        }]
+        .to_vec(),
+    });
+
+    return skillgems;
 }
